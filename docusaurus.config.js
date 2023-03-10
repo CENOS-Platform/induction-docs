93e138cee9f41a66001691c20697a898d91614bc
Raimonds Vilums <raimonds@cenos-platform.com>
2023-03-10 14:26:05 +0200

    Set Netlify url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://induction.cenos-platform.com',
+  url: 'https://induction-docs.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
